5413b836dad2c775a12124c752536f3adc6dc0a3
Владимир Крынин <krinin.vlad2804@icloud.com>
2025-01-04 10:35:33 +0700

    DS-131, fix build fails

--- src/app/(noprotected)/registration/page.tsx
+++ src/app/(noprotected)/registration/page.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { RegistationPage } from '../../views/Auth/RegistationPage';
+import { RegistationPage } from '../../../views/Auth/RegistationPage';
 
 export default function Registation() {
   return (
