682f3d856406ca049610170a4ac26d22cc8d9b0a
arrKhange1 <therock65frostik@gmail.com>
2025-01-05 13:19:52 +0500

    feat(DS-133): add jsdoc to container, layout

--- src/ui/ConstraintContainer/ConstaintContainer.tsx
+++ src/ui/ConstraintContainer/ConstaintContainer.tsx
@@ -1,11 +1,13 @@
 import clsx from 'clsx';
-
 import cls from './ConstraintContainer.module.scss';
 
 export interface IConstraintContainerProps extends React.PropsWithChildren {
   className?: string;
 }
 
+/**
+ * @description Контейнер, ограничивающий размер внутреннего контента
+ */
 export const ConstraintContainer: React.FC<IConstraintContainerProps> = ({ children, className }) => (
   <section className={clsx(cls.container, className)}>{children}</section>
 );
--- src/ui/ContentLayout/ContentLayout.tsx
+++ src/ui/ContentLayout/ContentLayout.tsx
@@ -5,6 +5,9 @@ export interface IContentLayoutProps extends React.PropsWithChildren {
   className?: string;
 }
 
+/**
+ * @description Обертка с переиспользуемыми стилями над разными компонентами
+ */
 export const ContentLayout: React.FC<IContentLayoutProps> = ({ className, children }) => (
   <section className={clsx(cls.layout, className)}>{children}</section>
 );
