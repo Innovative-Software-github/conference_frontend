c96d254e70a17a53620c321172b4e4bf7b5e0d7a
arrKhange1 <therock65frostik@gmail.com>
2025-01-06 14:33:21 +0500

    feat(DS-134): add filter for location

--- src/views/MainPage/components/Events/EventFilters/EventFilters.tsx
+++ src/views/MainPage/components/Events/EventFilters/EventFilters.tsx
@@ -3,8 +3,10 @@
 import { Calendar, ComboGroup, MultiSelect } from 'ui-kit-conf/dist';
 import { ISelectOptions } from 'ui-kit-conf/dist/types/components/Dropdown/Dropdown';
 import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import { ContentLayout } from '@/ui/ContentLayout/ContentLayout';
 import cls from './EventFilters.module.scss';
+import { IEventFilters } from '../Events';
 
 const cities: ISelectOptions[] = [
   { key: 'Москва', value: 'Москва' },
@@ -22,37 +24,57 @@ interface IEventDate {
 }
 
 export const EventFilters = () => {
-  const [selectedCities, setSelectedCities] = useState<ISelectOptions[]>([]);
+  // const [selectedCities, setSelectedCities] = useState<ISelectOptions[]>([]);
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
   const [eventDate, setEventDate] = useState<IEventDate>();
 
+  const { control, setValue, handleSubmit } = useForm<IEventFilters>({
+    defaultValues: {
+      location: [],
+      tags: [],
+      dateStart: null,
+      dateFinish: null,
+    },
+  });
+
+  const onSubmit = (eventFilters: IEventFilters) => console.log(eventFilters);
+
   return (
     <ContentLayout className={cls.fields}>
-      <section className={cls.mainFields}>
-        <MultiSelect
-          placeholder="Выберите города..."
-          options={cities}
-          selectedOptions={selectedCities}
-          onOptionClick={(cities) => setSelectedCities(cities)}
-        />
-        <Calendar
-          startDate={eventDate?.startDate ?? null}
-          endDate={eventDate?.endDate ?? null}
-          onChangeEndDate={(endDate) =>
-            setEventDate((prev) => ({ ...(prev ?? { endDate: null, startDate: null }), endDate }))
-          }
-          onChangeStartDate={(startDate) =>
-            setEventDate((prev) => ({ ...(prev ?? { endDate: null, startDate: null }), startDate }))
-          }
-        />
-      </section>
-      <ComboGroup isSorted defaultIds={selectedTags} onChange={(tags) => setSelectedTags(tags)}>
-        {tagOptions.map((tag) => (
-          <ComboGroup.Checkbox key={tag} id={tag}>
-            {tag}
-          </ComboGroup.Checkbox>
-        ))}
-      </ComboGroup>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <section className={cls.mainFields}>
+          <Controller
+            name="location"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <MultiSelect
+                placeholder="Выберите города..."
+                options={cities}
+                selectedOptions={value}
+                onOptionClick={(cities) => onChange(cities)}
+              />
+            )}
+          />
+          <Calendar
+            startDate={eventDate?.startDate ?? null}
+            endDate={eventDate?.endDate ?? null}
+            onChangeEndDate={(endDate) =>
+              setEventDate((prev) => ({ ...(prev ?? { endDate: null, startDate: null }), endDate }))
+            }
+            onChangeStartDate={(startDate) =>
+              setEventDate((prev) => ({ ...(prev ?? { endDate: null, startDate: null }), startDate }))
+            }
+          />
+        </section>
+        <ComboGroup isSorted defaultIds={selectedTags} onChange={(tags) => setSelectedTags(tags)}>
+          {tagOptions.map((tag) => (
+            <ComboGroup.Checkbox key={tag} id={tag}>
+              {tag}
+            </ComboGroup.Checkbox>
+          ))}
+        </ComboGroup>
+        <input type="submit" />
+      </form>
     </ContentLayout>
   );
 };
--- src/views/MainPage/components/Events/Events.tsx
+++ src/views/MainPage/components/Events/Events.tsx
@@ -1,3 +1,4 @@
+import { ISelectOptions } from 'ui-kit-conf/dist/types/components/Dropdown/Dropdown';
 import { ConstraintContainer } from '@/ui/ConstraintContainer/ConstaintContainer';
 import { EventFilters } from './EventFilters/EventFilters';
 import { EventList } from './EventList/EventList';
@@ -5,6 +6,13 @@ import { getEvents } from '@/services/events/request';
 import { IEventsResponse } from '@/services/events/interfaces';
 import { eventsFallback } from '@/constants/eventsFallback';
 import cls from './Events.module.scss';
+
+export interface IEventFilters {
+  location: ISelectOptions[];
+  tags: string[];
+  dateStart: Date | null;
+  dateFinish: Date | null;
+}
 
 export const Events = async () => {
   let events: IEventsResponse[] = eventsFallback;
