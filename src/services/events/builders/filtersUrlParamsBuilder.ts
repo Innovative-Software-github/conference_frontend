d78df2177c27515a4a2eda818c3f2f6ac23bfce3
arrKhange1 <therock65frostik@gmail.com>
2025-01-07 15:20:28 +0500

    fix(DS-134): fix builder logic

--- src/services/events/builders/filtersUrlParamsBuilder.ts
+++ src/services/events/builders/filtersUrlParamsBuilder.ts
@@ -1,17 +1,21 @@
-import { IEventFilters } from '@/views/MainPage/components/Events/Events';
+import { IEventFilters } from '../interfaces';
 
 export class FiltersUrlParamsBuilder {
   private filtersUrlParams = new URLSearchParams();
 
-  private addLocation(location: IEventFilters['location']): URLSearchParams {
-    if (!location) return this.filtersUrlParams;
-    console.log('location arr', location);
-    location.forEach((loc) => this.filtersUrlParams.append('location', loc.value));
-
+  private build(): URLSearchParams {
     return this.filtersUrlParams;
   }
 
+  private addLocation(location: IEventFilters['location']): FiltersUrlParamsBuilder {
+    if (!location) return this;
+    console.log('location arr', location);
+    location.forEach((loc) => this.filtersUrlParams.append('location', loc.value));
+
+    return this;
+  }
+
   builder(filters: IEventFilters): URLSearchParams {
-    return this.addLocation(filters.location);
+    return this.addLocation(filters.location).build();
   }
 }
