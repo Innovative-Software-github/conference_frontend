08193714bcfa815fe51592d05172ed7f40a2d4cd
Владимир Крынин <krinin.vlad2804@icloud.com>
2025-02-09 00:08:06 +0500

    DS-134, fix Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,15 @@ import { MainPage } from '../views/MainPage/MainPage';
 import { fetchFiltersConfig } from '../services/static/filtersConfig/serverAction';
 import { getEvents } from '../services/events/request';
 
-export const dynamic = 'force-dynamic';
+export interface IHome {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
 
 export default async function Home({
   searchParams,
-}: any) {
+}: IHome) {
   const filtersConfig = await fetchFiltersConfig();
   const events = await getEvents(await searchParams);
-
-  console.log(await searchParams)
 
   return <MainPage
     events={events.data}
